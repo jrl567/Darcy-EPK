5676cdc7afab8865250456a9cd9c39da6dc802d8
a1870771 <a1870771@adelaide.edu.au>
2026-04-17 00:12:20 +0930

    Overhaul

--- src/components/EpkFooter.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Footer() {
-  return(
-    <div className="border-t bg-[rbg(26,26,26)] w-full inline-flex items-center justify-center py-6 text-sm text-gray-300">
-          <p className="m-auto">Website by Jack Larkin ©2023</p>
-    </div>
-  );
-}
